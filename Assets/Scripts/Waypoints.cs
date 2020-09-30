96d220a01fabb2b53e75b8ade17158bc1363817f
Nerensio <nicolas.dumont84850@gmail.com>
2020-09-30 10:47:37 +0200

    moveEnemy

--- Assets/Scripts/Waypoints.cs
+++ Assets/Scripts/Waypoints.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Waypoints : MonoBehaviour
+ 
 {
+    public static Transform[] points;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        points = new Transform[transform.childCount];
+
+        for (int i = 0; i < points.Length; i++)
+            {
+            points[i] = transform.GetChild(i);
+            }
     }
 
     // Update is called once per frame
